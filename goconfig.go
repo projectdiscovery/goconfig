c8de611fad1f8da009c66eae2e8d592fc4faeb3c
mzack <marco.rivoli.nvh@gmail.com>
2021-06-30 20:13:06 +0200

    misc

--- goconfig.go
+++ goconfig.go
@@ -3,15 +3,20 @@ package goconfig
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
 
 	ini "gopkg.in/ini.v1"
 )
+
+func init() {
+	ini.PrettyFormat = false
+}
 
 // Save uses the same json taggers but convert it to INI for readability
 func Save(v interface{}, saveTo string) error {
 	var b []byte
 	// marshal to json then to map
-	b, err := json.Marshal(&b)
+	b, err := json.Marshal(v)
 	if err != nil {
 		return err
 	}
@@ -21,11 +26,17 @@ func Save(v interface{}, saveTo string) error {
 	if err != nil {
 		return err
 	}
-
 	// iterate over map setting key => value in ini file
 	cfg := ini.Empty()
 	for k, v := range kv {
-		cfg.Section("").NewKey(k, fmt.Sprint(v))
+		var data string
+		switch v.(type) {
+		case float64, float32:
+			data = strconv.FormatFloat(v.(float64), 'f', -1, 64)
+		default:
+			data = fmt.Sprint(v)
+		}
+		cfg.Section("").NewKey(k, data)
 	}
 	return cfg.SaveTo(saveTo)
 }
@@ -36,5 +47,7 @@ func Load(v interface{}, loadFrom string) error {
 	if err != nil {
 		return err
 	}
+	cfg.NameMapper = ini.TitleUnderscore
+
 	return cfg.MapTo(v)
 }
